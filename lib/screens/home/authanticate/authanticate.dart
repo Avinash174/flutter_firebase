bdca1c02549e288a141f06ae41ed1ac47fcbd043
Avinash175 <avinashmagar15@gmail.com>
2023-01-18 23:35:38 +0530

    New Changes Adding

---
+++ lib/screens/home/authanticate/authanticate.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class authanticate extends StatefulWidget {
+  const authanticate({Key? key}) : super(key: key);
+
+  @override
+  State<authanticate> createState() => _authanticateState();
+}
+
+class _authanticateState extends State<authanticate> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Wrraper extends StatelessWidget {
+  const Wrraper({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+
+  }
+}
